a2fb504d9027d668b3b5ced982cf6e6952edb3a8
alu0100813272 <alu0100813272@ull.edu.es>
2017-04-26 17:40:52 +0000

    fin

--- test/test_triangle.js
+++ test/test_triangle.js
@@ -7,14 +7,19 @@ let should = require('should');
 describe("Triangle", function() {
 
    it("Inicializacion", function() {
-        //var type = Triangle
-        //should.exist(new Triangle({width:15, height:15},type));
+        var type = Triangle
+        should.exist(new Triangle({width:15, height:15}));
+        
    })
   
-   it("Calculo del area triangulo", function() {
-        //var result = new Triangle({width:15, height:15}).area();
-        //result.should.equal(112.5);
+   it("Calculo del area triangle", function() {
+        var result = new Triangle({width:15, height:15}).area();
+        result.should.equal(112.5);
    })
    
+   it("Soy tipo triangle", function() {
+        var result = new Triangle({width:15, height:15}).gettipo();
+        result.should.equal('Triangle');
+   })
 });
          
